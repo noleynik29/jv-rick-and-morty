18b9044c3c7d14fbf1b792de86d113b970320aac
noleynik29 <noleynik29@gmail.com>
2026-02-10 11:28:21 +0200

    rick and morty characters homework done

--- src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -1,7 +1,7 @@
 package mate.academy.rickandmorty.repository;
 
-import mate.academy.rickandmorty.entity.Character;
 import java.util.List;
+import mate.academy.rickandmorty.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
